040ed26ac926a2bc8e07575ed786164b33d002f2
mohini-442 <mohinikarwasra598@gmail.com>
2023-11-17 16:01:14 -1200

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,23 @@
+function openNav() {
+    document.getElementById("navbar").classList.toggle("show");
+    document.body.classList.toggle("overflow_hidden");
+    document.getElementById("menubtn-icon").classList.toggle("cross")
+
+}
+// preloader------
+setTimeout(() => {
+    document.getElementById("preloader").classList.add("opacity-0");
+    document.getElementById("preloader").classList.add("pointer_event_none")
+    document.body.classList.remove("overflow-hidden")
+}, 2000);
+
+// back to top
+window.addEventListener('scroll', function () {
+    let backtotop = document.getElementById("backtotop")
+    if (window.scrollY > 300) {
+        backtotop.style.display = 'block'
+    }
+    else {
+        backtotop.style.display = 'none'
+    }
+})
